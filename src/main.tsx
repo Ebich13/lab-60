f6e9a0e736e99f0af1f9a796e24b36c55ba3a919
Kubat <kubas9214@gmail.com>
2023-11-20 18:25:55 +0600

    Config commit!

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import 'react-bootstrap/dist/react-bootstrap.js';
+import 'bootstrap/dist/css/bootstrap.css';
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>,
-)
+);
